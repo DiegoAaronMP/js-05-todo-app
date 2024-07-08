0375a5d7d7ee302862a16435c6fd2baf481acc69
DiegoAaronMP <daaron.macias@gmail.com>
2024-07-08 17:14:53 -0600

    Métodos que ocuparemos del Store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,57 @@ const initStore = () => {
     console.log('InitStore 🥑');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * Crear tarea
+ * @param {String} description Descripción de la tarea
+ */
+const addTodo = ( description = '' ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * Cambiar estado de tarea
+ * @param {String} todoId Identidicador único de la tarea
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * Eliminar una tarea
+ * @param {String} todoId Identidicador único de la tarea
+ */
+const deleteTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * Eliminar las tareas completadas
+ */
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
